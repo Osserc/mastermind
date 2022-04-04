5cf4015ad37bd64e69e9f7342c80d6cf4d9570c6
Stefano Rondelli <rstefanofil@hotmail.com>
2022-04-04 13:32:18 +0200

    Add code input
    Both the player and the computer can act as codemaker:
    Put checks in place to assess the validity of the input

--- mastermind.rb
+++ mastermind.rb
@@ -1,10 +1,32 @@
+module Validation
+    RANGE = [1, 2, 3, 4, 5, 6]
+    def check_code(code)
+        code.all? { | integer | RANGE.include?(integer)} && code.length == 4
+    end
+end
+
 class Game
-    
+    attr_reader :range
+    include Validation
+
+
     def play_game
         player_one = make_player_one()
         player_two = make_player_two(player_one)
-        puts player_one.breaker
-        puts player_two.breaker
+        human, computer = find_human_player(player_one, player_two)
+        # now we ask what the code to guess is
+        if human.breaker == "no"
+            puts "Please enter the code you wish your opponent to break"
+            code_to_break = human.make_code
+            until check_code(code_to_break) == true do
+                puts "Invalid code; please insert a valid code."
+                code_to_break = human.make_code
+            end
+            puts code_to_break
+        else
+            code_to_break = computer.make_code
+            puts code_to_break.join
+        end
     end
 
 
@@ -25,14 +47,49 @@ class Game
            player_two = Player.new("yes")
        end
     end
+
+    def find_human_player(player_one, player_two)
+        if player_one.human == 0
+            return player_one, player_two
+        else
+            return player_two, player_one
+        end
+    end
+
 end
 
 class Player
-    attr_reader :breaker
+    attr_reader :breaker, :human
+    include Validation
+    @@human = 0
 
-    def initialize(breaker)
+    def initialize(breaker, human = @@human)
         @breaker = breaker
+        @human = human
+        @@human += 1
     end
+
+    def make_code()
+        if self.human == 0
+            puts "Insert your 4-numbered code."
+            code = gets.chomp.split('').map { | character | character.to_i}
+        else
+            code = RANGE.sample(4)
+        end
+    end
+
+    def guess_code(player)
+
+    end
+end
+
+class Code
+    # attr_reader :
+
+    def initialize(code)
+
+    end
+
 end
 
 Game.new.play_game
